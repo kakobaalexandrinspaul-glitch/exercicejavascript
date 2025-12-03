346fadc2d9aa166ddf93f95ba7d330835db38f73
alexandrinspaulkakobajr <kakobaalexandrinspaul@gmail.com>
2025-12-03 14:08:25 +0200

    premier exercicemathoperation resolu

---
+++ mathOperations.js
@@ -0,0 +1,17 @@
+let nombre1 = prompt("Entrez le premier nombre :");
+let nombre2 = prompt("Entrez le deuxième nombre :");
+
+nombre1 = Number(nombre1);
+nombre2 = Number(nombre2);
+
+
+let somme = nombre1 + nombre2;
+let difference = nombre1 - nombre2;
+let produit = nombre1 * nombre2;
+let division = nombre1 / nombre2;
+
+
+console.log("Somme :", somme);
+console.log("Différence :", difference);
+console.log("Produit :", produit);
+console.log("Division :", division);
